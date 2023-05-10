e0a01d700705e8de37cdb54a6e9ee3834f0691b5
bamboos-alex <124706455+bamboos-alex@users.noreply.github.com>
2023-05-10 19:08:06 +0900

    LDM-181 roadNo, direction 으로 a7 생성하기 추가

--- bb_geo_db.py
+++ bb_geo_db.py
@@ -2,43 +2,53 @@ import mariadb
 import geojson
 import json
 
-def findA2Link(a2LinkId):
-    conn = mariadb.connect(
-        user="ldm",
-        password="ldm",
-        host="127.0.0.1",
-        database="next-ldm"
-    )
+# def findA2Link(request):
+#     conn = mariadb.connect(
+#         user="ldm",
+#         password="ldm",
+#         host="127.0.0.1",
+#         database="next-ldm"
+#     )
     
-    cur = conn.cursor()
-    
-    if a2LinkId: 
-        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and id = '" + a2LinkId + "';") 
-    else:
-        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 10000 order by id asc;")
-        # cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where (hdMapType = 'A2_LINK') and (JSON_VALUE(properties, '$.roadNo') = '1') and (ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 1000)  order by id asc;")
- 
-    linkList = []
-    for id, properties, geometry in cur :
-        print(f"id={id}, properties={properties}, geometry={geometry}")
+#     cur = conn.cursor()
 
-        prop = json.loads(properties)
-        geo = geojson.loads(geometry)
+#     if (isinstance(request, dict)):
+#         roadNo = request['roadNo']
+#         direction = request['direction']
+#         cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' "
+#                     + " and JSON_VALUE(properties, \"$.roadNo\") = '" + roadNo + "' "
+#                     + " and JSON_VALUE(properties, \"$.direction\") = '" + direction + "' order by id asc;") 
+#     elif request : 
+#         a2LinkId = request
+#         cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and id = '" + a2LinkId + "';") 
+#     else:
+#         cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 10000 order by id asc;")
+#         # cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where (hdMapType = 'A2_LINK') and (JSON_VALUE(properties, '$.roadNo') = '1') and (ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 1000)  order by id asc;")
+ 
+#     linkList = []
+#     for id, properties, geometry in cur :
+#         print(f"id={id}, properties={properties}, geometry={geometry}")
+
+#         prop = json.loads(properties)
+#         geo = geojson.loads(geometry)
          
-        link = {"id": id, "properties": prop, "geometry": geo}
-        linkList.append(link)
+#         link = {"id": id, "properties": prop, "geometry": geo}
+#         linkList.append(link)
         
     
-    conn.close()
+#     conn.close()
 
-    return linkList
+#     return linkList
     
     
 # a2Link = findA2Link()
 # a2Link = findA2Link(a2LinkId="A217BR720009")
+
+# roadNoAndDirection = {"roadNo": "50", "direction": "E"}
+# a2Link = findA2Link(request=roadNoAndDirection)
 # print(a2Link)
 
-def findA7LinkSlice(a7Id):
+def findLink(request):
     conn = mariadb.connect(
         user="ldm",
         password="ldm",
@@ -47,13 +57,33 @@ def findA7LinkSlice(a7Id):
     )
     
     cur = conn.cursor()
+
+    # cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and JSON_VALUE(properties, '$.direction') IS NULL order by id asc;") 
+    # cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A2_LINK' and JSON_VALUE(properties, '$.roadNo') = '1' order by id asc;") 
     
-    if a7Id: 
-        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A7_LINK_SLICE' and id = '" + a7Id + "';") 
+    if (isinstance(request, dict)):
+        roadNo = request['roadNo']
+        direction = request['direction']
+        hdMapType = request['hdMapType']
+
+        if (not roadNo):
+             cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = '" + hdMapType + "' "
+            + " and JSON_VALUE(properties, \"$.roadNo\") IS NULL order by id asc;") 
+        elif (direction) :
+            cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = '" + hdMapType + "' "
+            + " and JSON_VALUE(properties, \"$.roadNo\") = '" + roadNo + "' "
+            + " and JSON_VALUE(properties, \"$.direction\") = '" + direction + "' order by id asc;") 
+        else :
+            cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = '" + hdMapType + "' "
+            + " and JSON_VALUE(properties, \"$.roadNo\") = '" + roadNo + "' order by id asc;") 
+    elif request : 
+        id = request
+        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = '" + hdMapType +  "' and id = '" + id + "';") 
     else:
-        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A7_LINK_SLICE' order by id asc;") 
-        #cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A7_LINK_SLICE' and ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 10000 order by id asc;")
-        # cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where (hdMapType = 'A7_LINK_SLICE') and (JSON_VALUE(properties, '$.roadNo') = '1') and (ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 1000)  order by id asc;")
+        cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = '" + hdMapType + "' order by id asc;") 
+    #     # cur.execute("select id, properties, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A7_LINK_SLICE' order by id asc;") 
+    #     # cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where hdMapType = 'A7_LINK_SLICE' and ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 10000 order by id asc;")
+    #     # cur.execute("select id, ST_AsGeoJSON(geometry2D) from HD_MAP hm where (hdMapType = 'A7_LINK_SLICE') and (JSON_VALUE(properties, '$.roadNo') = '1') and (ST_DISTANCE(ST_GEOMFROMTEXT('POINT(332020.85 4128721.928)'), geometry2D) < 1000)  order by id asc;")
  
     linkList = []
     for id, properties, geometry in cur :
@@ -76,3 +106,11 @@ def findA7LinkSlice(a7Id):
 # a7Link = findA7LinkSlice(a7Id="A219AG640002_A7000001")
 # print(a7Link)
 
+# a7Request = {"roadNo": "50", "direction": "E", "hdMapType": "A7_LINK_SLICE"}
+# a7Link = findLink(request=a7Request)
+# print(a7Link)
+
+# a2Request = {"roadNo": "50", "direction": "E", "hdMapType": "A2_LINK"}
+# a2Request = {"roadNo": None, "direction": "E", "hdMapType": "A2_LINK"}
+# a2Link = findLink(request=a2Request)
+# print(a2Link)
--- bb_geo_view.py
+++ bb_geo_view.py
@@ -3,7 +3,7 @@ import json
 import geojson
 
 from bb_geo_tran import UTM52N_To_LatLon
-from bb_geo_db import findA2Link, findA7LinkSlice
+from bb_geo_db import findLink
 from geojson import MultiLineString, Feature, FeatureCollection
 
 
@@ -11,14 +11,20 @@ from geojson import MultiLineString, Feature, FeatureCollection
 #     geo_xy = json.loads(f.read())
 #     f.close()
 
-def create_feature_collection(a2LinkList):
+def create_feature_collection(linkList):
     feature_list = []
     index = 1
-    for a2Link in a2LinkList:
-        id = a2Link['id']
-        properties = a2Link['properties']
+    for link in linkList:
+        id = link['id']
+        properties = link['properties']
         properties['index'] = index
-        geometry = a2Link['geometry']
+        geometry = link['geometry']
+
+        if ('roadNo' not in properties):
+            properties['roadNo'] = 'NULL'
+
+        if ('direction' not in properties):
+            properties['direction'] = 'NULL'
 
         feature = Feature(properties=properties, geometry=geometry, id=id)
         feature_list.append(feature)
@@ -38,17 +44,55 @@ def convert_geojson_to_folium(tuple):
 
 ## main ##
 
-a2linkList = findA2Link("")
+roadNo_50_Direction_E = {"roadNo": "50", "direction": "E"}
+roadNo_50_Direction_S = {"roadNo": "50", "direction": "S"}
+roadNo_50 = {"roadNo": "50", "direction": ""}
+roadNo_1_Direction_E = {"roadNo": "1", "direction": "E"}
+roadNo_1_Direction_S = {"roadNo": "1", "direction": "S"}
+roadNo_1 = {"roadNo": "1", "direction": ""}
+roadNo_null = {"roadNo": None, "direction": None}
+roadNo_100_Direction_E = {"roadNo": "100", "direction": "E"}
+roadNo_100_Direction_S = {"roadNo": "100", "direction": "S"}
+
+
+# a2LinkList_50_E = findA2Link(request=roadNo_50_Direction_E)
+# a2LinkList_50_S = findA2Link(request=roadNo_50_Direction_S)
+
+a2Reuqset = {"hdMapType": "A2_LINK"}
+a2Reuqset.update(roadNo_1)
+a2LinkList_1 = findLink(request=a2Reuqset)
+
+a2Reuqset.update(roadNo_50)
+a2LinkList_50 = findLink(request=a2Reuqset)
+
+# a2LinkList_1_E = findA2Link(request=roadNo_1_Direction_E)
+# a2LinkList_1_S = findA2Link(request=roadNo_1_Direction_S)
+# a2LinkList_100_E = findA2Link(request=roadNo_100_Direction_E)
+# a2LinkList_100_S = findA2Link(request=roadNo_100_Direction_S)
+
+a2LinkList = []
+a2LinkList.extend(a2LinkList_1)
+a2LinkList.extend(a2LinkList_50)
+# a2LinkList = a2LinkList_50_E
+# a2LinkList.extend(a2LinkList_50_S)
+# a2LinkList.extend(a2LinkList_1_E)
+# a2LinkList.extend(a2LinkList_1_S)
+# a2LinkList.extend(a2LinkList_100_E)
+# a2LinkList.extend(a2LinkList_100_S)
+
+# a2linkList = findA2Link("")
 # a2linkList = findA2Link("A217BR720009")
 # print (a2linkList)
 
-geo_xy = create_feature_collection(a2linkList)
+# geo_xy = create_feature_collection(a2LinkList_1_E)
+geo_xy = create_feature_collection(a2LinkList)
 geo_latlon = geojson.utils.map_tuples(convert_geojson_to_folium, geo_xy)
 
 m = folium.Map(
     location=UTM52N_To_LatLon(332020.85, 4128721.928), # 신갈 JC
-    zoom_start=17,
-    tiles=""
+    zoom_start=10,
+    # tiles=""
+    tiles="OpenStreetMap"
 )
 
 ### A2 LINK Layer
@@ -56,27 +100,44 @@ m = folium.Map(
 style_function = lambda x: {
     "color": "#3388ff" if int(x["properties"]["index"])%2 == 0 else "#80B5FF"
 }
-tooltip = folium.GeoJsonTooltip(fields=['id', 'roadType', 'roadNo', 'direction', 'laneNo', 'maxSpeed', 'length','roadRank','linkType',  'fromNodeId', 'toNodeId',  'index'])
+a2_fields=['id', 'roadType', 'roadNo', 'direction', 'laneNo', 'maxSpeed', 'length','roadRank','linkType',  'fromNodeId', 'toNodeId',  'index']
+tooltip = folium.GeoJsonTooltip(fields=a2_fields)
+popup = folium.GeoJsonPopup(fields=a2_fields)
 
 folium.GeoJson(
     data=geo_latlon,
     style_function=style_function,
     name='A2_LINK',
     overlay=True,
-    tooltip=tooltip
+    tooltip=tooltip,
+    popup=popup
 ).add_to(m)
 
 
 ### A7 LINK SLICE Layer
+a7Request = {"hdMapType": "A7_LINK_SLICE"}
+a7Request.update(roadNo_1)
+a7LinkList_1 = findLink(request=a7Request)
+a7Request.update(roadNo_50)
+a7LinkList_50 = findLink(request=a7Request)
+# a7LinkList_50_E = findA7Link(request=roadNo_50_Direction_E)
+# a7LinkList_50_S = findA7Link(request=roadNo_50_Direction_S)
+# a7LinkList_1_E = findA7Link(request=roadNo_1_Direction_E)
+# a7LinkList_1_S = findA7Link(request=roadNo_1_Direction_S)
+# a7LinkList_100_E = findA7Link(request=roadNo_100_Direction_E)
+# a7LinkList_100_S = findA7Link(request=roadNo_100_Direction_S)
 
-a7LinkSliceList = findA7LinkSlice("")
+a7LinkList = []
+a7LinkList.extend(a7LinkList_1)
+a7LinkList.extend(a7LinkList_50)
+# a7LinkList.extend(a7LinkList_50_E)
+# a7LinkList.extend(a7LinkList_50_S)
+#a7LinkList.extend(a7LinkList_1_E)
+#a7LinkList.extend(a7LinkList_1_S)
+# a7LinkList.extend(a7LinkList_100_E)
+# a7LinkList.extend(a7LinkList_100_S)
+
 # a7LinkSliceList = findA7LinkSlice("A219AG640002_A7000001")
-
-# style_function_other = lambda x: {
-#     #"color": "#FF0000" if int(x["properties"]["indexInA2Link"])%2 == 0 else "#00FF00"
-#     "color": "#FF0000" if int(x["properties"]["index"])%2 == 0 else "#00FF00"
-# }
-
 
 def style_function_other(x):
     color = "#FF0000"
@@ -84,13 +145,21 @@ def style_function_other(x):
     a7_prop = x["properties"]
 
     
-    print (a7_prop)
+    #print (a7_prop)
     
     if (a7_prop["roadType"] == "MAIN_ROAD") :
-        if (a7_prop["index"] % 2 == 0):
-            color = "#66CDAA"
+
+        if (a7_prop["length"] == 100):        
+            if (a7_prop["index"] % 2 == 0):
+                color = "#66CDAA"
+            else:
+                color = "#BADA55"
         else:
-            color = "#D3FFCE"
+            if (a7_prop["index"] % 2 == 0):
+                color = "#B6FCD5"
+            else:
+                color = "#D3FFCE"
+            
     else:
         if (a7_prop["index"] % 2 == 0):
             color = "#FA8072" 
@@ -100,18 +169,22 @@ def style_function_other(x):
     return {"color": color}
 
 # geo_a7slice_xyz = geojson.loads(open("./a7slice_youngin_4km.geojson", 'r').read())
-geo_a7slice_xyz = create_feature_collection(a7LinkSliceList)
+geo_a7slice_xyz = create_feature_collection(a7LinkList)
 geo_a7slice_latlon = geojson.utils.map_tuples(convert_geojson_to_folium, geo_a7slice_xyz)
-geo_tooltip = folium.GeoJsonTooltip(fields=['id', 'roadType','roadNo', 'direction', 'laneNo', 'maxSpeed','length', 'prev', 'next', 'a2LinkId',  'indexInA2Link',  'minRow', 'maxRow',  'minColumn',  'maxColumn', 'index'])
+
+a7_fields=['id', 'roadType','roadNo', 'direction', 'laneNo', 'maxSpeed','length', 'prev', 'next', 'a2LinkId',  'indexInA2Link',  'minRow', 'maxRow',  'minColumn',  'maxColumn', 'index']
+geo_tooltip = folium.GeoJsonTooltip(fields=a7_fields)
+geo_popup = folium.GeoJsonPopup(fields=a7_fields)
 
 folium.GeoJson(
     data=geo_a7slice_latlon,
     style_function=style_function_other,
     name="A7_LINK_SLICE",
     overlay=True,
-    tooltip=geo_tooltip
+    tooltip=geo_tooltip,
+    popup=geo_popup
 ).add_to(m)
 
-folium.LayerControl().add_to(m)
+folium.LayerControl(position='topright', collapsed=False).add_to(m)
 
-m.save('bb-map.html')
+m.save('bb-map-bg-road-number-1-50.html')
