cefb863fc4c49453cad1faa7aec989b2b087e932
bamboos-alex <124706455+bamboos-alex@users.noreply.github.com>
2023-05-16 13:09:46 +0900

    LDM-213 RoadNo 1, 5 Layer 구분하여 html 만들기

--- bb_geo_view.py
+++ bb_geo_view.py
@@ -3,13 +3,12 @@ import json
 import geojson
 
 from bb_geo_tran import UTM52N_To_LatLon
-from bb_geo_db import find
+from bb_geo_db import find, find_manual_sql
 from geojson import MultiLineString, Feature, FeatureCollection
+from folium import Marker
 
 
-# with open('./A217BR720009.json', mode='rt', encoding='utf-8') as f:
-#     geo_xy = json.loads(f.read())
-#     f.close()
+HTML_FILE_NAME='bb-map-bg-a7-road-number-1-50.html'
 
 def create_feature_collection(linkList):
     feature_list = []
@@ -44,6 +43,16 @@ def convert_geojson_to_folium(tuple):
 
 ## main ##
 
+m = folium.Map(
+    location=UTM52N_To_LatLon(332020.85, 4128721.928), # 신갈 JC
+    zoom_start=10,
+    # tiles=""
+    tiles="OpenStreetMap"
+)
+
+
+## A2 Link
+
 roadNo_50_Direction_E = {"roadNo": "50", "direction": "E"}
 roadNo_50_Direction_S = {"roadNo": "50", "direction": "S"}
 roadNo_50 = {"roadNo": "50", "direction": ""}
@@ -55,45 +64,27 @@ roadNo_100_Direction_E = {"roadNo": "100", "direction": "E"}
 roadNo_100_Direction_S = {"roadNo": "100", "direction": "S"}
 
 
-# a2LinkList_50_E = findA2Link(request=roadNo_50_Direction_E)
-# a2LinkList_50_S = findA2Link(request=roadNo_50_Direction_S)
+a2Request_1 = {"hdMapType": "A2_LINK"}
+a2Request_1.update(roadNo_1)
+a2LinkList_1 = find(request=a2Request_1)
 
-a2Reuqset = {"hdMapType": "A2_LINK"}
-a2Reuqset.update(roadNo_1)
-a2LinkList_1 = find(request=a2Reuqset)
+a2Request_50 = {"hdMapType": "A2_LINK"} 
+a2Request_50.update(roadNo_50)
+a2LinkList_50 = find(request=a2Request_50)
 
-a2Reuqset.update(roadNo_50)
-a2LinkList_50 = find(request=a2Reuqset)
 
-# a2LinkList_1_E = findA2Link(request=roadNo_1_Direction_E)
-# a2LinkList_1_S = findA2Link(request=roadNo_1_Direction_S)
-# a2LinkList_100_E = findA2Link(request=roadNo_100_Direction_E)
-# a2LinkList_100_S = findA2Link(request=roadNo_100_Direction_S)
-
-a2LinkList = []
-a2LinkList.extend(a2LinkList_1)
-a2LinkList.extend(a2LinkList_50)
-# a2LinkList = a2LinkList_50_E
-# a2LinkList.extend(a2LinkList_50_S)
-# a2LinkList.extend(a2LinkList_1_E)
-# a2LinkList.extend(a2LinkList_1_S)
-# a2LinkList.extend(a2LinkList_100_E)
-# a2LinkList.extend(a2LinkList_100_S)
-
-# a2linkList = findA2Link("")
-# a2linkList = findA2Link("A217BR720009")
-# print (a2linkList)
+print("#############################################")
+print (a2LinkList_1)
+print("#############################################")
+print (a2LinkList_50)
+print("#############################################")
 
 # geo_xy = create_feature_collection(a2LinkList_1_E)
-geo_xy = create_feature_collection(a2LinkList)
-geo_latlon = geojson.utils.map_tuples(convert_geojson_to_folium, geo_xy)
+geo_xy_1 = create_feature_collection(a2LinkList_1)
+geo_latlon_1 = geojson.utils.map_tuples(convert_geojson_to_folium, geo_xy_1)
 
-m = folium.Map(
-    location=UTM52N_To_LatLon(332020.85, 4128721.928), # 신갈 JC
-    zoom_start=10,
-    # tiles=""
-    tiles="OpenStreetMap"
-)
+geo_xy_50 = create_feature_collection(a2LinkList_50)
+geo_latlon_50 = geojson.utils.map_tuples(convert_geojson_to_folium, geo_xy_50)
 
 ### A2 LINK Layer
 
@@ -101,43 +92,47 @@ style_function = lambda x: {
     "color": "#3388ff" if int(x["properties"]["index"])%2 == 0 else "#80B5FF"
 }
 a2_fields=['id', 'roadType', 'roadNo', 'direction', 'laneNo', 'maxSpeed', 'length','roadRank','linkType',  'fromNodeId', 'toNodeId',  'index']
-tooltip = folium.GeoJsonTooltip(fields=a2_fields)
-popup = folium.GeoJsonPopup(fields=a2_fields)
+
+tooltip_1 = folium.GeoJsonTooltip(fields=a2_fields)
+popup_1 = folium.GeoJsonPopup(fields=a2_fields)
+
+tooltip_50 = folium.GeoJsonTooltip(fields=a2_fields)
+popup_50 = folium.GeoJsonPopup(fields=a2_fields)
 
 folium.GeoJson(
-    data=geo_latlon,
+    data=geo_latlon_1,
     style_function=style_function,
-    name='A2_LINK',
+    name='A2_LINK_RoadNo_1',
     overlay=True,
-    tooltip=tooltip,
-    popup=popup
+    tooltip=tooltip_1,
+    popup=popup_1
+).add_to(m)
+
+folium.GeoJson(
+    data=geo_latlon_50,
+    style_function=style_function,
+    name='A2_LINK_RoadNo_50',
+    overlay=True,
+    tooltip=tooltip_50,
+    popup=popup_50
 ).add_to(m)
 
 
+
 ### A7 LINK SLICE Layer
-a7Request = {"hdMapType": "A7_LINK_SLICE"}
-a7Request.update(roadNo_1)
-a7LinkList_1 = find(request=a7Request)
-a7Request.update(roadNo_50)
-a7LinkList_50 = find(request=a7Request)
-# a7LinkList_50_E = findA7Link(request=roadNo_50_Direction_E)
-# a7LinkList_50_S = findA7Link(request=roadNo_50_Direction_S)
-# a7LinkList_1_E = findA7Link(request=roadNo_1_Direction_E)
-# a7LinkList_1_S = findA7Link(request=roadNo_1_Direction_S)
-# a7LinkList_100_E = findA7Link(request=roadNo_100_Direction_E)
-# a7LinkList_100_S = findA7Link(request=roadNo_100_Direction_S)
+a7Request_1 = {"hdMapType": "A7_LINK_SLICE"}
+a7Request_1.update(roadNo_1)
+a7LinkList_1 = find(request=a7Request_1)
 
-a7LinkList = []
-a7LinkList.extend(a7LinkList_1)
-a7LinkList.extend(a7LinkList_50)
-# a7LinkList.extend(a7LinkList_50_E)
-# a7LinkList.extend(a7LinkList_50_S)
-#a7LinkList.extend(a7LinkList_1_E)
-#a7LinkList.extend(a7LinkList_1_S)
-# a7LinkList.extend(a7LinkList_100_E)
-# a7LinkList.extend(a7LinkList_100_S)
+a7Request_50 = {"hdMapType": "A7_LINK_SLICE"}
+a7Request_50.update(roadNo_50)
+a7LinkList_50 = find(request=a7Request_50)
 
-# a7LinkSliceList = findA7LinkSlice("A219AG640002_A7000001")
+print("#############################################")
+print(a7LinkList_1);
+print("#############################################")
+print(a7LinkList_50)
+print("#############################################")
 
 def style_function_other(x):
     color = "#FF0000"
@@ -168,23 +163,61 @@ def style_function_other(x):
 
     return {"color": color}
 
-# geo_a7slice_xyz = geojson.loads(open("./a7slice_youngin_4km.geojson", 'r').read())
-geo_a7slice_xyz = create_feature_collection(a7LinkList)
-geo_a7slice_latlon = geojson.utils.map_tuples(convert_geojson_to_folium, geo_a7slice_xyz)
 
 a7_fields=['id', 'roadType','roadNo', 'direction', 'laneNo', 'maxSpeed','length', 'prev', 'next', 'a2LinkId',  'indexInA2Link',  'minRow', 'maxRow',  'minColumn',  'maxColumn', 'index']
-geo_tooltip = folium.GeoJsonTooltip(fields=a7_fields)
-geo_popup = folium.GeoJsonPopup(fields=a7_fields)
+geo_tooltip_1 = folium.GeoJsonTooltip(fields=a7_fields)
+geo_popup_1 = folium.GeoJsonPopup(fields=a7_fields)
+
+
+geo_tooltip_50 = folium.GeoJsonTooltip(fields=a7_fields)
+geo_popup_50 = folium.GeoJsonPopup(fields=a7_fields)
+
+geo_a7slice_xyz_1 = create_feature_collection(a7LinkList_1)
+geo_a7slice_latlon_1 = geojson.utils.map_tuples(convert_geojson_to_folium, geo_a7slice_xyz_1)
+
+geo_a7slice_xyz_50 = create_feature_collection(a7LinkList_50)
+geo_a7slice_latlon_50 = geojson.utils.map_tuples(convert_geojson_to_folium, geo_a7slice_xyz_50)
 
 folium.GeoJson(
-    data=geo_a7slice_latlon,
+    data=geo_a7slice_latlon_1,
     style_function=style_function_other,
-    name="A7_LINK_SLICE",
+    name="A7_LINK_SLICE_RoadNo_1",
     overlay=True,
-    tooltip=geo_tooltip,
-    popup=geo_popup
+    tooltip=geo_tooltip_1,
+    popup=geo_popup_1
 ).add_to(m)
+
+folium.GeoJson(
+    data=geo_a7slice_latlon_50,
+    style_function=style_function_other,
+    name="A7_LINK_SLICE_RoadNo_50",
+    overlay=True,
+    tooltip=geo_tooltip_50,
+    popup=geo_popup_50
+).add_to(m)
+
+
+# IC/JC point 
+print("#############################################")
+points = find_manual_sql();
+for point in points:
+    print(point)
+
+   # print(point['geometry']['coordinates'])
+    (lon, lat) = convert_geojson_to_folium(point['geometry']['coordinates'])
+    
+    location = [lat, lon]
+    name=point['properties']['name']
+    id=point['id']
+    #popup='id=' + id + ' name='+ name
+    properties = point['properties']
+    popup=json.dumps(properties, indent=2)
+
+   # print(location)
+
+    Marker(location=location, popup=popup).add_to(m)
+
 
 folium.LayerControl(position='topright', collapsed=False).add_to(m)
 
-m.save('bb-map-bg-road-number-1-50.html')
+m.save(HTML_FILE_NAME)
