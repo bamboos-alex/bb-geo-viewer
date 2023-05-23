1fbbfe8cffbaabb3962fa0fc65b1bc4326a27549
bamboos-alex <124706455+bamboos-alex@users.noreply.github.com>
2023-05-23 15:54:49 +0900

    LDM-227 지역 이름으로 파일 생성 개선

--- bb_geo_view_seoul.py
+++ bb_geo_view_seoul.py
@@ -9,17 +9,19 @@ from folium import Marker
 
 
 meter = 3000 # meter
-INIT_POINT = (313732.3108338943, 4161109.817388392) # 상암동 사거리 126.890584 37.578210
+#INIT_POINT = (313732.3108338943, 4161109.817388392) # 상암동 사거리 126.890584 37.578210
+INIT_POINT = (316605.046633, 4155231.821346) # 여의대로 126.9245642 37.5259637
 
-flag_with_bg = True
+flag_with_bg = False
 pre_fix='a7'
+name='yeouido'
 #post_fix='road-rank-not-1'
 post_fix=''
 
 if (flag_with_bg) :
-    HTML_FILE_NAME='bb-map-bg-' + pre_fix + '-sangam-' + str(meter) + '-meter' + post_fix + '.html'
+    HTML_FILE_NAME='bb-map-bg-' + pre_fix + '-' + name + '-' + str(meter) + '-meter' + post_fix + '.html'
 else :
-    HTML_FILE_NAME='bb-map-' + pre_fix + '-sangam-' + str(meter) + '-meter' + post_fix + '.html'
+    HTML_FILE_NAME='bb-map-' + pre_fix + '-' + name + ' -' + str(meter) + '-meter' + post_fix + '.html'
 
 # main ##
 
